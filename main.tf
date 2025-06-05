04b723aab760742457a6d3ea71126e191f06aa04
Ayush Sharma <ayushshrama88@gmail.com>
2025-06-05 20:40:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-0aeeebd8d2ab47354" # Amazon Linux 2 (us-east-1)
   instance_type = "t2.micro"
 
   tags = {
