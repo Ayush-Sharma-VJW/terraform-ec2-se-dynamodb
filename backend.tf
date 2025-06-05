4ee654629c98e4568049006f428b03fee99a60fc
Ayush Sharma <ayushshrama88@gmail.com>
2025-06-05 20:37:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-ayush-demo"      
     key            = "terraform/state/ec2.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "terraform-locks"
   }
 }
